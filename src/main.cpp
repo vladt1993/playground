7df0bd832c4444d562809e6d0339ce4439e8bcc7
Taller <taller@lx007511.rsint.net>
2026-02-10 12:12:05 +0100

    feat: initial stracture

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    std::cout << "Hello Conan\n";
+    return 0;
+}
---
+++ tests/testExample.cpp
@@ -0,0 +1,6 @@
+#include <gtest/gtest.h>
+
+TEST(SampleTest, BasicAssertion)
+{
+    EXPECT_EQ(1 + 1, 2);
+}
